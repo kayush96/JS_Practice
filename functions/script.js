f98adb57324c997efe2fc3b7324febfa7594fd84
Ayush Kumar Verma <engiworks2016@gmail.com>
2022-05-28 16:49:54 +0530

    Add Functions Program

---
+++ functions/script.js
@@ -0,0 +1,16 @@
+//Make a function named logger that console.log the argument you passed into it.
+function logger(str1) {
+    console.log(str1);
+}
+//Make a function called add that adds two numbers together.
+function add(num1, num2) {
+    return num1 + num2;
+}
+//Make a function called multiply that multiplies two numbers together.
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+console.log(logger("hello"));
+console.log(add(2, 5));
+console.log(multiply(5, 10));
