50121499290646a17f53a41eaba8d0bfb80278e1
Ayush Kumar Verma <engiworks2016@gmail.com>
2022-05-29 01:52:49 +0530

    Add Arrays Code

---
+++ arrays/script.js
@@ -0,0 +1,14 @@
+//Make an empty array that contains nothing.
+const myArray = [];
+//Make an array that contains three items.
+const newArray = ["Apple", "Banana", "Mango"];
+//Given this a list of people (below), do the following:
+let people = [
+    'Franklin Roosevelt',
+    'Thomas Edison',
+    'Benjamin Franklin'
+];
+    //Get Franklin Roosevelt from the people array
+    console.log(people[0]);
+    //Set Thomas Edison to Inventor of the lightbulb
+    console.log(people[1]);
