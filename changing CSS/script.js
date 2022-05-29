44ab751ca8b3ff2639f1db1606657a202605f09a
Ayush Kumar Verma <engiworks2016@gmail.com>
2022-05-29 22:28:27 +0530

    Add CSS Change with JS

---
+++ changing CSS/script.js
@@ -0,0 +1,14 @@
+//Create a button. Do the following when the button is clicked:
+const button = document.querySelector(".btn");
+//Change the button’s color
+button.addEventListener('click', event => {
+    button.style.color = "red";
+    button.style.backgroundColor = "black";
+    button.style.width = "100px";
+    button.style.height = "50px";
+});
+//Change the button’s backgroundColor
+
+//Change the button’s width
+
+//Change the button’s height
